746be9e5bd8b49b01c97a108d00be091935650bf
Matthew Geniza <matthew.geniza@gmail.com>
2017-11-27 09:29:50 -0800

    Create Binpacker_assembly.sh

---
+++ Binpacker_assembly.sh
@@ -0,0 +1,8 @@
+BinPacker -s <read_type> -p <sequencing_type> -l <left_reads> -r <right_reads> -d -o <output_diretory> -m <library_type>
+## where -s is "fq" (fastq) or "fa" (fasta)
+## where -p is "pair" or "single"
+## where -l is the name and path of the left reads
+## where -r is the name and path of the right reads
+## where -d removes duplicated transcripts
+## where -o is the name and path of the output_directory
+## where -m is the library type orientation - for pair, either "RF" or "FR" - for single, either "R" or "F"
