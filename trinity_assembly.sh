34f870ce8e9bf3e67b81fead915afc713b59580d
Matthew Geniza <matthew.geniza@gmail.com>
2017-11-27 09:40:16 -0800

    Create trinity_assembly.sh

---
+++ trinity_assembly.sh
@@ -0,0 +1,5 @@
+Trinity --seqType fq --left <left_reads> --right <right_reads> --output test-trinity-cotton 
+## where --seqType is read type "fq" fastq or "fa" fasta
+## where --left is name and location of left reads
+## where --right is name and location of right reads
+## where --output is the name and location output
