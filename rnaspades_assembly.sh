cd56aa3a471cedb0b68090c4e4afa38f2791d296
Matthew Geniza <matthew.geniza@gmail.com>
2017-11-27 09:35:12 -0800

    Create rnaspades_assembly.sh

---
+++ rnaspades_assembly.sh
@@ -0,0 +1,4 @@
+rnaspades.py -1 <forward_reads> -2 <reverse_reads> -o <location_output>
+## where -1 is the name and path of forward reads
+## where -2 is the name and path of reverse_reads
+## where -o is the path of where the output will be placed
